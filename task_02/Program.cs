70d5daaef6832d78aa5dbbef65ba302c38553888
Grigory3v <jack977j@gmail.com>
2022-07-28 19:26:29 +0400

    Добавил task_02

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -1,24 +1,34 @@
 ﻿/*
-Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 
-452 -> 11
-
-82 -> 10
-
-9012 -> 12
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
 */
 
-int Sum_digits_number()
+namespace Task_02
 {
-    int sum = 0;
-    Console.WriteLine("Введите число");
-    int namber = Convert.ToInt32(Console.ReadLine());
-    string str = Convert.ToString(namber);
-    for (int i = 0; i < str.Length; i++)
+    class Program
     {
-        sum += Convert.ToInt32(str[i].ToString());
+        public static void Main()
+        {
+            Console.WriteLine("Введите число M");
+            int M = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            number(M: M, N: N);
+
+        }
+        static void number(int M, int N)
+        {
+            int sum = 0;
+            for (; M <= N; M++)
+            {
+                sum += M;
+            }
+            Console.Write($"{sum} ");
+        }
+
     }
-    return sum;
 }
-int sum = Sum_digits_number();
-Console.WriteLine($"сумма цифр в числе = {sum}");
